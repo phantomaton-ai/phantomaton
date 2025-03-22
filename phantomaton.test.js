5f5e04be4338deefbeee0f563dac48cc9b5c401d
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-22 13:12:34 -0700

    roo

--- phantomaton.test.js
+++ phantomaton.test.js
@@ -15,17 +15,18 @@ Testbot!
 
 describe('Phantomaton', () => {
   let start;
+  const installs = {
+    'start-plugin': [
+      priestess.start.provider([], () => start)
+    ]
+  };
+  const lookup = async (module) => ({ default: () => ({
+    install: installs[module] || []
+  }) });
 
   beforeEach(() => {
     start = stub();
-    const installs = {
-      'start-plugin': [
-        priestess.start.provider([], () => start)
-      ]
-    };
-    stub(Importer.prototype, 'import').callsFake(module => ({ default: () => ({
-      install: installs[module] || []
-    }) }));
+    stub(Importer.prototype, 'import').callsFake(lookup);
   });
 
   afterEach(() => {
@@ -37,6 +38,18 @@ describe('Phantomaton', () => {
     expect(Importer.prototype.import.calledWith('start-plugin')).to.be.true;
     expect(Importer.prototype.import.calledWith('conf-plugin')).to.be.false;
   });
+
+  it('accepts a root configuration', async () => {
+    let root;
+    const oldStub = Importer.prototype.import;
+    Importer.prototype.import = function (m) {
+      root = this.root;
+      return oldStub(m);
+    };
+    await phantomaton(TEST, '/tmp/foo');
+    expect(root).to.equal('/tmp/foo');
+    Importer.prototype.import = oldStub;
+  })
 
   it('imports configured modules too', async () => {
     await phantomaton(TEST, { install: ['conf-plugin'] });
